3afc349fafa39a67dc2cc2f9160f51cbc81772e6
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-09-24 00:45:36 +0530

    first commit

--- server.js
+++ server.js
@@ -1,25 +1,33 @@
-import express from 'express'
-import dotenv from 'dotenv'
+import express from "express";
+import dotenv from "dotenv";
 import morgan from "morgan";
+import cors from "cors";
+import testRoutes from "./routes/testRoutes.js";
 
-import testRouters from "./routes/testRoutes.js"
+//conig env var
 dotenv.config();
 
-const app = express()
+//rest object
+const app = express();
 
-const PORT = process.env.PORT || 5000
+//middlewares
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
 
+//routes
+app.use("/api/v1/test", testRoutes);
 
-app.use('/api/v1/test', testRouters)
+app.get("/", (req, res) => {
+  res.send("<h1> Node Server Running </h1>");
+});
 
-app.use(express.json())
-app.use(morgan('dev'));
+//port
+const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => res.send('Hello World'))
-
-app.post('/', (req, res) => {
-    console.log(req.body)
-    res.json({ success: true })
-})
-
-app.listen(PORT, () => console.log(`Server is running ${process.env.NODE_ENV} Port ${PORT}`))
+//run server
+app.listen(PORT, () => {
+  console.log(
+    `Node Server Ruuning in ${process.env.NODE_ENV} Mode On Port ${PORT}`
+  );
+});
