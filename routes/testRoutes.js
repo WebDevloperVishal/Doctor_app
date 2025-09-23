36d0a84c4dbaa7846cd4f651e426d1607c3be380
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-09-24 00:17:49 +0530

    first commit

--- routes/testRoutes.js
+++ routes/testRoutes.js
@@ -4,7 +4,7 @@ import { getTestController } from '../controllers/testController.js';
 // import all controllers
 // import SessionController from './app/controllers/SessionController';
 
-const routes = express.Routes();
+const routes = express.Router();
 
 // Add routes
 routes.get('/', getTestController);
