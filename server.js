ccb4e7510ea7d765583b3ddd64fc9618b7027bfe
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-09-24 00:04:13 +0530

    first commit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = 8080
 
 app.use(express.json())
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World'))
 
 app.post('/', (req, res) => {
     console.log(req.body)
