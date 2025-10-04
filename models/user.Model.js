e6d1d37a9158847b4d0f8857a09a26fa45a2db8f
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-10-04 20:26:44 +0530

    user Model

---
+++ models/user.Model.js
@@ -0,0 +1,23 @@
+import mongoose, { Types } from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        require: [true, 'name is required']
+    },
+    password: {
+        type: String,
+        require: [true, 'password is required']
+    },
+    email: {
+        type: String,
+        require: [true, 'email is required'],
+        unique: true
+    },
+    image: { type: String },
+    phone: { type: String },
+    address: { type: String },
+    dob: { type: String },
+    gender: { type: String },
+    isAdmin: { type: Boolean, default: false }
+}, { timestamps: true })
