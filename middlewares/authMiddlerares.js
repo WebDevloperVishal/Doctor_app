f89f724ea54ac7434bc74aa51f0496f830a14ca1
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-10-06 19:56:05 +0530

    first commit

--- middlewares/authMiddlerares.js
+++ middlewares/authMiddlerares.js
@@ -27,7 +27,15 @@ export const userAuth = async (req, res, next) => {
 // Admon Auth
 export const isAdmin = async (req, res, next) => {
     try {
-     const user = await userModel   
+     const user = await userModel.findById(req.user.id)
+     if(user.isAdmin === !true){
+        return res.status(402).send({
+            success:false,
+            message: "Unatuhorized Access"
+        })
+     } else {
+        next();
+     }
     } catch (error) {
        console.log(error),
        res.status(402).send({
