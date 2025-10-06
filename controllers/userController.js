0268e710d671a70055369a8918a4c4b4c7be88ee
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-10-06 20:11:35 +0530

    first commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -89,51 +89,6 @@ export const userLogin = async(req, res) => {
         });
     }
 }
-// export const userLogin = async (req, res) => {
-//     try {
-//         const { email, password } = req.body;
-//         // validation
-
-//         if(!email || !password){
-//             return res.status(400).send({
-//                 success:false,
-//                 message:"Provide All Fields"
-//             })
-//         }
-
-//         // check if user exists
-//         const user = await userModel.findOne({ email });
-//         if(!user){
-//             return res.status(404).send({
-//                 success:false,
-//                 message:"User Not Found"
-//             })
-//         }
-
-//         // compare password
-//         const isMatch = await bcrypt.compare(password, user.password);
-//         if(!isMatch){
-//             return res.status(401).send({
-//                 success:false,
-//                 message:"Invalid Credentials"
-//             })
-//         }
-
-//         res.status(200).send({
-//             success: true,
-//             message:"User Login Successfully",
-//             user
-//         });
-//     } catch (error) {
-//         console.log(error);
-//         res.status(500).send({
-//             success: false,
-//             message: 'Something Went Wrong',
-//             error
-//         });
-//     }
-// }
-
 
 // Get User
 export const getUserProfile = async (req, res) => {
