52c8975fc6320f7f797b734852663a0b2f78410c
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-10-15 20:23:12 +0530

    message model done

---
+++ models/webMessage.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const webMessageSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'name is required']
+    },
+    contact: {
+        type: String,
+        required: [true, 'contact no or email is required']  // ✅ fixed
+    },
+    message: {
+        type: String,
+        required: [true, 'message is required']
+    },
+}, { timestamps: true });
+
+const webmessageModel = mongoose.model('webmessage', webMessageSchema);
+
+export default webmessageModel;
