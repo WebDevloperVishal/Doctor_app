41986da1c298be310b750db5102166cc099da8ef
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-10-23 20:26:10 +0530

    Message

--- controllers/webMessageController.js
+++ controllers/webMessageController.js
@@ -1,20 +1,20 @@
 import webmessageModel from "../models/webMessage.js";
 
 // create Message
-export const createMessage = async (req , res)=> {
+export const createMessage = async (req, res) => {
     try {
-        const {name, contact,message} = req.body
-        if(!name || !contact || !message){
+        const { name, contact, message } = req.body
+        if (!name || !contact || !message) {
             return res.status(402).send({
-                success:false,
-                message:'Provide All Fields'
+                success: false,
+                message: 'Provide All Fields'
             })
         }
-        const webMessage = new webmessageModel({name,contact,message})
+        const webMessage = new webmessageModel({ name, contact, message })
         webMessage.save()
         res.status(201).send({
-            success:true,
-            message:'Message Send Successfully',
+            success: true,
+            message: 'Message Send Successfully',
             webMessage,
         })
     } catch (error) {
@@ -26,3 +26,50 @@ export const createMessage = async (req , res)=> {
         });
     }
 }
+
+// getAll messsage
+export const getAllMessage = async (req, res) => {
+    try {
+        const webMessage = await webmessageModel.find({})
+        res.status(201).send({
+            success: true,
+            message: 'get All message',
+            totalCount: webMessage.length,
+            webMessage,
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            message: "Error in get all Web Message Api",
+            error
+        });
+    }
+}
+
+
+// Delete messsage
+export const DeleteMessage = async (req, res) => {
+    try {
+        const { id } = req.params
+        if (!id)
+            return res.status(404).send({
+                success: false,
+                message: 'Provide valid Message id'
+            })
+        //find message
+        const webMessage = await webmessageModel.findByIdAndDelete(id)
+        res.status(201).send({
+            success: true,
+            message: "Message has been deleted",
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            message: "Error in get all Web Message Api",
+            error
+        });
+    }
+}
+
--- routes/webMessageRoutes.js
+++ routes/webMessageRoutes.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { createMessage } from "../controllers/webMessageController.js";
+import { createMessage, DeleteMessage, getAllMessage } from "../controllers/webMessageController.js";
+import { isAdmin, userAuth } from "../middlewares/authMiddlerares.js";
 
 const routes = express.Router()
 
 // create Message ||POST
-routes.post('/create',createMessage)
+routes.post('/create',createMessage);
+
+routes.get('/getallmsg',getAllMessage);
+
+routes.delete('/deletemsg/:id',userAuth,isAdmin,DeleteMessage);
 
 export default routes;
